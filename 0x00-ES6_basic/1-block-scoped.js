e08db3dbffdc7ea5c9ac0d957547fec91bbcc155
Ibrodante <ibroman02@yahoo.com>
2023-09-30 11:06:12 +0000

    update

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-  const task = true; //eslint-disabled
-  const task2 = false; // estlint-disable
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
