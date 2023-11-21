67e07ed45d28389e5345bf40191c5819fa44789a
IbroDante <53307395+IbroDante@users.noreply.github.com>
2023-11-21 23:26:18 +0100

    Update StudentsController.js

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -7,7 +7,7 @@ const VALID_MAJORS = ['CS', 'SWE'];
 
 /**
  * Contains the student-related route handlers.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author ibrahim balogun <https://github.com/ibrodante>
  */
 class StudentsController {
   static getAllStudents(request, response) {
